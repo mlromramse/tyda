bfb5f92885b4971a1895f4a31b9cd4feac07267d
Mikael Larsson <mikael.larsson@romram.se>
2022-02-21 13:17:17 +0100

    bug: fixed changed origin page

--- tyda.py
+++ tyda.py
@@ -11,40 +11,47 @@ class Tyda():
 
     def tyd(self, word):
         html = requests.get('http://tyda.se/search/{}?lang%5B0%5D=en&lang%5B1%5D=sv'.format(word)).content
-        # print html
+        # print(html)
         soup = BeautifulSoup(html, 'html.parser')
-        search_results = soup.select('div.box-searchresult')
+        search_results = soup.select('div.box-searchresult > div.box')
         for search_result in search_results:
-            name_elem = search_result.select_one('h1 > b')
+            name_elem = search_result.select_one('h1')
             if not name_elem:
-                name_elem = search_result.select_one('h2 > b')
+                name_elem = search_result.select_one('h2')
+            if not name_elem:
+                continue
             print (name_elem.get_text().strip())
             synonyms = search_result.select('ul.list-synonyms li.item')
             if synonyms:
                 print ('   SYNONYMS')
                 for synonym in synonyms:
-                    text = synonym.select_one('h3 > a').get_text().strip()
+                    text_elem = synonym.select_one('h3 > a')
+                    text = ''
+                    if text_elem:
+                        text = text_elem.get_text().strip()
                     desc = ''
                     desc_elem = synonym.select_one('span.syn-desc')
                     if desc_elem:
                         desc = desc_elem.get_text().strip()
                     print ('    - ', text, desc)
-            language = search_result.select_one('ul.list-translations li.item-title')
+            language = search_result.select_one('ul.list-translations li.item-title span img')
+            word_class = search_result.select_one('div.word-class')
             translations = search_result.select('ul.list-translations li.item > a')
             if translations:
-                print ('   TRANSLATIONS')
+                print ('   TRANSLATIONS - ' + word_class.get_text().strip())
                 if language:
-                    print ('      ' + language.get_text().strip())
+                    print ('      ' + language['title'])
                 for translation in translations:
                     print ('       - ' + translation.get_text().strip())
 
     def did_you_mean(self, word):
         response = requests.get('http://tyda.se/complete?word={}&lang%5B%5D=en&lang%5B%5D=sv'.format(word))
         response_json = response.json()
+        # print(response_json)
         if response_json['status'] == 1:
             print ('\nDid you mean:')
             for item in response_json['items']:
-                print ('   ' + item['name'])
+                print ('   ' + item['langid'])
                 for word in item['words']:
                     print ('    - ' + word['word'])
         print('')
